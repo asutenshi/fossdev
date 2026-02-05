161ccf5a57b7d07ef5028d07f64b91aab116ffd8
tenshi <zakan.6000@gmail.com>
2026-02-05 15:18:55 +0800

    Added phantom string

--- script.py
+++ script.py
@@ -1,9 +1,10 @@
 def sum(a, b):
-  if not (isinstance(a, int) or isinstance(b, int)):
-    raise ValueError("Summing could be with integers")
-  return a + b
+    if not (isinstance(a, int) or isinstance(b, int)):
+        raise ValueError("Summing could be with integers")
+    return a + b
+
 
 def devide(a, b):
-  if b == 0:
-    raise ValueError("Denominator could not be zero")
-  return a / b
+    if b == 0:
+        raise ValueError("Denominator could not be zero")
+    return a / b
