f82dcc2a9a005ff490629220e904773276eb393e
tenshi <zakan.6000@gmail.com>
2026-02-05 15:17:51 +0800

    Added tests

---
+++ test.py
@@ -0,0 +1,31 @@
+from script import devide, sum
+
+
+def test_sum():
+    a = 1
+    b = 2
+    result = 3
+    assert sum(a, b) == result
+
+
+def test_devide():
+    a = 2
+    b = 4
+    result = 0.5
+    assert devide(a, b) == result
+
+
+def test_devide_zero():
+    a = 2
+    b = 0
+    try:
+        sum(a, b)
+        assert False
+    except ValueError as e:
+        print("Test (zero-devision) passed")
+
+
+if __name__ == "__main__":
+    test_sum()
+    test_devide()
+    test_devide_zero()
