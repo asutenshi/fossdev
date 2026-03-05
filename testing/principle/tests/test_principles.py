a28782ab19b69f31cf5614da783b8dad383eaeca
asutenshi <zakan.6000@gmail.com>
2026-03-05 13:16:54 +0800

    Show test-clusters principle

--- testing/principle/tests/test_principles.py
+++ testing/principle/tests/test_principles.py
@@ -35,9 +35,21 @@ def test_addition_overkill():
             assert add(i, -j) == i + -j
 
 
+def test_addition_clussters():
+    assert add(7, 6) == 13
+    assert add(0, 6) == 6
+    assert add(7, 0) == 7
+    assert add(10, -11) == -1
+    assert add(-10, -11) == -21
+    assert add(0, -5) == -5
+    assert add(-10, 0) == -10
+    print("Test CLUSSTERS ADDITION PASSED")
+
+
 if __name__ == "__main__":
     test_addition()
     test_addition_with_bug()
     test_addition_duplicate()
     # test_addition_overkill()
+    test_addition_clussters()
     
