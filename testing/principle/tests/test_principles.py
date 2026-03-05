e2b86dde4a21034a71ccb6ef1f65ff447be0de5f
asutenshi <zakan.6000@gmail.com>
2026-03-05 13:03:55 +0800

    Added do-not-predict-func-principle

--- testing/principle/tests/test_principles.py
+++ testing/principle/tests/test_principles.py
@@ -19,6 +19,12 @@ def test_addition_with_bug():
     print("Test BUGGED ADDITION PASSED")
 
 
+def test_addition_duplicate():
+    assert add(6, 7) == 6 + 7
+    print("Test DUPLICATE ADDITION PASSED")
+
+
 if __name__ == "__main__":
     test_addition()
     test_addition_with_bug()
+    test_addition_duplicate()
