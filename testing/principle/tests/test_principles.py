e548a47b52310de12789e5bd30fffce2518238bd
asutenshi <zakan.6000@gmail.com>
2026-03-05 12:37:46 +0800

    Remove kostil

--- testing/principle/tests/test_principles.py
+++ testing/principle/tests/test_principles.py
@@ -1,11 +1,13 @@
-import sys
-sys.path.append("../src")
+# import sys
+# sys.path.append("../src")
 
 from math_demo import add
+
 
 def test_addition():
     assert 2 + 2 == 4
     print("Test ADDITION PASSED")
+
 
 if __name__ == "__main__":
     test_addition()
