2e673d03e8696a3d3634ad4d3fd283ccd53584dd
asutenshi <zakan.6000@gmail.com>
2026-03-05 12:29:59 +0800

    Initial commit for testing parctice. Added test for addition

---
+++ testing/principle/tests/test_principles.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append("../src")
+
+from math_demo import add
+
+def test_addition():
+    assert 2 + 2 == 4
+    print("Test ADDITION PASSED")
+
+if __name__ == "__main__":
+    test_addition()
