3108606a608f79a3d0f57b6d62238f55e6d0dc66
Dest1n640 <realmadridfunca@gmail.com>
2026-03-29 18:01:12 +0800

    Finish, delete venv.py file

--- makeutil/python-hw/make/venv.mk
+++ makeutil/python-hw/make/venv.mk
@@ -4,19 +4,19 @@ PIP = $(VENV)/bin/pip
 
 # 1
 make_venv:
-    python3 -m venv $(VENV)
+	python3 -m venv $(VENV)
 
 install_dep: make_venv
-    $(PIP) install -r requirements.txt
+	$(PIP) install -r requirements.txt
 
 install: install_dep
-    $(PIP) install -e .
+# 	$(PIP) install -e .
 
 start_file:
-    $(PYTHON) src/app.py
+	$(PYTHON) src/app.py
 
 test: install start_file
 
 # 2
-chech-requirements:
-	$(PYTHON) check_requirements.py
+check-requirements: make_venv
+	$(PYTHON) tools/check_requirements.py
